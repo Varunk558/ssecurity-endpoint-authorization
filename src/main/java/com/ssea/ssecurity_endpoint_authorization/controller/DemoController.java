6fea3c195c21795e7defaf6335e7ec58bc7a5679
Varun <varunkothapally18@gmail.com>
2025-10-29 12:10:52 -0500

    Created DemoController with the required endpoints

---
+++ src/main/java/com/ssea/ssecurity_endpoint_authorization/controller/DemoController.java
@@ -0,0 +1,29 @@
+package com.ssea.ssecurity_endpoint_authorization.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @GetMapping("/demo1")
+    public String demo1(){
+
+        return "Demo1!!";
+    }
+
+    @GetMapping("/demo2")
+    public String demo2(){
+
+        return "Demo2!!";
+    }
+
+    @PostMapping("/demo3")
+    public String demo3(){
+        return "Demo3";
+    }
+}
